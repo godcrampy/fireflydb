954ecaf544a1b0f49d46f2ae35adece4c102cc20
Sahil Bondre <sahilbondre@gmail.com>
2023-12-28 17:50:05 +0000

    ffdb-005: add synchronized factory for DB

--- src/main/java/com/sahilbondre/firefly/FireflyDB.java
+++ src/main/java/com/sahilbondre/firefly/FireflyDB.java
@@ -1,5 +1,22 @@
 package com.sahilbondre.firefly;
 
-public class FireflyDB {
+import java.util.HashMap;
+import java.util.Map;
 
+public class FireflyDB {
+    private static final Map<String, FireflyDB> instances = new HashMap<>();
+    private final String folderPath;
+
+    private FireflyDB(String folderPath) {
+        this.folderPath = folderPath;
+    }
+
+    public static synchronized FireflyDB getInstance(String folderPath) {
+        instances.computeIfAbsent(folderPath, FireflyDB::new);
+        return instances.get(folderPath);
+    }
+
+    public String getFolderPath() {
+        return folderPath;
+    }
 }
